c799c2eaabb0d8889a5bec7ed58fd9fa970f6000
Nick Hart <nhartmusic@gmail.com>
2021-05-10 18:14:30 +0200

    better menu query

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 export default ({ pageContext }) => (
   <div>
-      <h1>{pageContext.title}</h1>
+    <Layout>
+      <h1 dangerouslySetInnerHTML={{ __html: pageContext.title }} />
+    </Layout>
   </div>
 )
